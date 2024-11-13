db22d550336a4401d29e61b83c0d886ccff1d9a1
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 04:58:00 +0700

    add triple points logic closes #63

--- 2-CreateRunCSConsoleApp/2-3-TripleDiceGameApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-3-TripleDiceGameApp/Program.cs
@@ -13,8 +13,8 @@ int roll1 = die.Next(1,7);
 int roll2 = die.Next(1,7);
 int roll3 = die.Next(1,7);
 
-// roll1=10;
-// roll2=10;
+roll1=10;
+roll2=10;
 // roll3=10;
 int total = roll1 + roll2 + roll3;
 
@@ -22,16 +22,22 @@ System.Console.WriteLine($"Total = roll1 + roll2 + roll3 = {roll1} + {roll2} + {
 
 if (roll1 == roll2 || roll1 == roll3 || roll2 == roll3)
 {
-  total += 6;
+  total += 2; // #62
+  System.Console.WriteLine("Double trouble! Bonus 2 points!");
+}
+
+if (roll1 == roll2 && roll2 == roll3)
+{
+  total += 6; // #63
+  System.Console.WriteLine("You be trippin! Bonus 6 points!");
 }
 
 if (total >= 15) // #60 15+ points = win
 {
-  System.Console.WriteLine("Winner winner, 🐔 dinner!");
+  System.Console.WriteLine($"Winner winner, 🐔 dinner! ({total} points)");
 }
 
 if (total < 15) // #61 <15 points = lose
 {
-  System.Console.WriteLine("You lost, please try again! 👎");
+  System.Console.WriteLine($"You lost, please try again! 👎 ({total} points)");
 }
-
