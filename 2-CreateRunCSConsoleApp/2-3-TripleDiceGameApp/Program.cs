1880ae21cec264fe0b839cca4ae856ae0601ad8b
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 04:26:58 +0700

    add winner logic

--- 2-CreateRunCSConsoleApp/2-3-TripleDiceGameApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-3-TripleDiceGameApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Welcome to Tony's Die Game 🎲🎲🎲`! ");
+Console.WriteLine("Welcome to Tony's Die Game 🎲🎲🎲! ");
 
 // 1. create die
 // 2. roll thrice
@@ -15,3 +15,10 @@ int roll3 = die.Next(1,7);
 int total = roll1 + roll2 + roll3;
 
 System.Console.WriteLine($"Total = roll1 + roll2 + roll3 = {roll1} + {roll2} + {roll3} = {total}");
+
+// #60 15+ points = win
+
+if (total >= 15)
+{
+  System.Console.WriteLine("Winner winner, 🐔 dinner!");
+}
