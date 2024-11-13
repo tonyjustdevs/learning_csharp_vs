7fcf15c3b585dd5ee20a526502350e94fc3c32ea
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 05:09:48 +0700

    remove stacking bonus closes #65

--- 2-CreateRunCSConsoleApp/2-3-TripleDiceGameApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-3-TripleDiceGameApp/Program.cs
@@ -1,5 +1,8 @@
-﻿
-Console.WriteLine("Welcome to Tony's Die Game 🎲🎲🎲! ");
+﻿Console.WriteLine("");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("--- Welcome to Tony's Dice-Roll App ---");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("");
 
 // 1. create die
 // 2. roll thrice
@@ -12,23 +15,25 @@ int roll2 = die.Next(1,7);
 int roll3 = die.Next(1,7);
 
 roll1=1;
-roll2=1;
-// roll3=10;
+roll2=10;
+roll3=10;
 int total = roll1 + roll2 + roll3;
 
 System.Console.WriteLine($"Total = roll1 + roll2 + roll3 = {roll1} + {roll2} + {roll3} = {total}");
 
 if (roll1 == roll2 || roll1 == roll3 || roll2 == roll3)
 {
-  total += 2; // #62
-  System.Console.WriteLine("Double trouble! Bonus 2 points!");
+  if (roll1 == roll2 && roll2 == roll3)
+  {
+    total += 6; // #63
+    System.Console.WriteLine("You be trippin! Bonus 6 points!");
+  } else
+  {
+    total += 2; // #62
+    System.Console.WriteLine("Double trouble! Bonus 2 points!");
+  }
 }
 
-if (roll1 == roll2 && roll2 == roll3)
-{
-  total += 6; // #63
-  System.Console.WriteLine("You be trippin! Bonus 6 points!");
-}
 
 if (total >= 15) // #60 15+ points = win
 {
@@ -38,3 +43,10 @@ else // #61 <15 points = lose
 {
   System.Console.WriteLine($"You lost, please try again! 👎 ({total} points)");
 }
+
+
+Console.WriteLine("");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("--- Leaving Tony's Dice-Roll App ------");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("");
