dc1279c3c87815f2fc1cad91cd98318a7e328bf9
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-13 07:20:04 +0700

    add BadRandomResultApp

---
+++ 2-CreateRunCSConsoleApp/2-2-BadRandomResultApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// Call an instance method on the class itself - Expected: Compilation Error
+
+System.Console.WriteLine(Random.Next(1,7));
+// Random die = new();
+// System.Console.WriteLine(die.Next(1,7));
