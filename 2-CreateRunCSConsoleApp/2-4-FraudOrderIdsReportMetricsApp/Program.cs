ab9253806826703a42c719d6bed9f1c6e1c1cc32
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 10:53:15 +0700

    reassign first fraud order id and output - closes #100

--- 2-CreateRunCSConsoleApp/2-4-FraudOrderIdsReportMetricsApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-4-FraudOrderIdsReportMetricsApp/Program.cs
@@ -13,6 +13,9 @@ System.Console.WriteLine($"First: {FraudulentOrderIds[0]}");
 System.Console.WriteLine($"Second: {FraudulentOrderIds[1]}");
 System.Console.WriteLine($"Third: {FraudulentOrderIds[2]}");
 
+FraudulentOrderIds[0] = "TottenhamAnge2ndYear";
+System.Console.WriteLine($"Re-assigned First: {FraudulentOrderIds[0]}");
+
 Console.WriteLine("");
 Console.WriteLine("-------------------------------------------");
 Console.WriteLine("--- Leaving Tony's Fraud-Metrics App ------");
