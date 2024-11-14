592ac7feed0667d251b7fd47ae608a71d2e68c3d
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 11:59:12 +0700

    initialise cat names arrays - closes #108

---
+++ 2-CreateRunCSConsoleApp/2-4-CatNameLoopererApp/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("--- Welcome to Tony's Cat Names App ---");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("");
+
+// #108  cat names array
+// #109  for each loop
+
+string[] CatNames = {"Oreo","Lilo","Stitch"};
+string[] CatNames2 = ["Mono","Ollie","Lucky"];
+
+System.Console.WriteLine(CatNames[0]);
+System.Console.WriteLine(CatNames[1]);
+System.Console.WriteLine(CatNames[2]);
+System.Console.WriteLine("");
+System.Console.WriteLine(CatNames2[0]);
+System.Console.WriteLine(CatNames2[1]);
+System.Console.WriteLine(CatNames2[2]);
+
+Console.WriteLine("");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("--- Leaving Tony's Cat Names App ------");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("");
+
