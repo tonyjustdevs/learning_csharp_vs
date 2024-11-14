94d509900e25543ee0dd320baf13d76c05cc58bf
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 10:27:59 +0700

    create multiple fraud ids at variable declaration - closes #96

---
+++ 2-CreateRunCSConsoleApp/2-4-MultipleFraudOrderIdsOneLinerApp/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("--- Welcome to Tony's Fraud Order App -");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("");
+
+// string[] FraudulentOrderIds = new string[3];
+string[] FraudulentOrderIds = ["A069","B420","C666"];
+
+System.Console.WriteLine(FraudulentOrderIds[0]);
+System.Console.WriteLine(FraudulentOrderIds[1]);
+System.Console.WriteLine(FraudulentOrderIds[2]);
+
+Console.WriteLine("");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("--- Leaving Tony's Fraud Order App ----");
+Console.WriteLine("---------------------------------------");
+Console.WriteLine("");
+
