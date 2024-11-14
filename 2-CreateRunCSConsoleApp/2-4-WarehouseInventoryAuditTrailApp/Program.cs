d4d15796e30f33828c4431ed8cfb7ddc790c1253
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 11:40:18 +0700

    initialise inventory array param - closes #104

---
+++ 2-CreateRunCSConsoleApp/2-4-WarehouseInventoryAuditTrailApp/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("--- [Tony's Warehouse Inventory Audit App] ---");
+Console.WriteLine("-------------------------------==-------------");
+Console.WriteLine("");
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+
+System.Console.WriteLine($"inventory type: {inventory}");
+Console.WriteLine("");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("--- [Application Ended] ----------------------");
+Console.WriteLine("----------------------------------------------");
+Console.WriteLine("");
+
