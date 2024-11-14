eda6247bdfe4e924e577a1df101ca6b05b28ae3f
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 12:32:50 +0700

    add inventory sum - closes #105

--- 2-CreateRunCSConsoleApp/2-4-WarehouseInventoryAuditTrailApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-4-WarehouseInventoryAuditTrailApp/Program.cs
@@ -5,8 +5,16 @@ Console.WriteLine("-------------------------------==-------------");
 Console.WriteLine("");
 
 int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
 
-System.Console.WriteLine($"inventory type: {inventory}");
+
+foreach (int product in inventory)
+{
+  // System.Console.WriteLine(product);
+  sum += product;
+}
+System.Console.WriteLine($"Total Products: {sum}");
+
 Console.WriteLine("");
 Console.WriteLine("----------------------------------------------");
 Console.WriteLine("--- [Application Ended] ----------------------");
