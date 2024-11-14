85e87b54571888d99876a3ac6e9d353e0db9acd8
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 07:59:00 +0700

    add rules conditionals framework closes #77

--- 2-CreateRunCSConsoleApp/2-3-ImproveRenewalRateOfSubscriptionsApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-3-ImproveRenewalRateOfSubscriptionsApp/Program.cs
@@ -10,8 +10,31 @@ Random random = new Random();
 int daysToExp = random.Next(12);
 int discountPct = 0;
 
-System.Console.WriteLine(daysToExp);
-System.Console.WriteLine(discountPct);
+// Rule 1: Your code should only display one message.
+// The message that your code displays will depend on the other five rules. 
+// For rules 2-6, the higher numbered rules take precedence over the lower numbered rules.
+
+
+// rule 1: add if-else with 5 possibilities (rules 2 to 6) #77
+if (true) // rule 2:
+{
+  System.Console.WriteLine($"2");
+} else if (true) // rule 3:
+{
+  System.Console.WriteLine($"3");
+} else if (true) // rule 4:
+{
+  System.Console.WriteLine($"4");
+  
+} else if (true) // rule 5:
+{
+  System.Console.WriteLine($"5");
+  
+} else // rule 6:
+{
+  System.Console.WriteLine($"6");
+}
+
 
 Console.WriteLine("");
 Console.WriteLine("------------------------------------------");
