7d3bb8a53818f1f670fd3a1d94019909c94981e4
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-14 10:50:50 +0700

    create fraud report (top 3 ids) - closes #99

---
+++ 2-CreateRunCSConsoleApp/2-4-FraudOrderIdsReportMetricsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("--- Welcome to Tony's Fraud-Metrics App ---");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("");
+
+// #99  eg report: First: "A420" etc
+// #100 eg report: Reassigned First: "A999" etc
+// #101 eg report: Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+string[] FraudulentOrderIds = ["ManCity115","Arsenal2nd","Liverpool3rd"];
+
+System.Console.WriteLine($"First: {FraudulentOrderIds[0]}");
+System.Console.WriteLine($"Second: {FraudulentOrderIds[1]}");
+System.Console.WriteLine($"Third: {FraudulentOrderIds[2]}");
+
+Console.WriteLine("");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("--- Leaving Tony's Fraud-Metrics App ------");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("");
+
