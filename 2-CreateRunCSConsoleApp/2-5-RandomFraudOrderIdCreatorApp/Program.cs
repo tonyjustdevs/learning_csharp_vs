a7343a963d4ca09bf9f476c3da627708b87b1c82
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-16 03:51:46 +0700

    add blank order ids and loop through - close #115

---
+++ 2-CreateRunCSConsoleApp/2-5-RandomFraudOrderIdCreatorApp/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("--- [Tony's Fraud-Order-Id-Generator App] ---");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("");
+
+// Loop through each blank orderID #115
+// 115-1 - Create empty array OrderIDs of length 5
+// 115-2 - Loop through each orderID
+// Get a random value that equates to ASCII letters A through E #116
+// Convert the random value into a char, then a string #117
+// Create a random number, pad with zeroes #118
+// Combine the prefix and suffix together, then assign to current OrderID #119
+// Print out each orderID #120
+
+// 115-2 - Create empty array OrderIDs of length 5
+
+string[] OrderIDs = new string[5];
+int row = 1;
+foreach (string orderID in OrderIDs) // 115-1 - Loop through each blank orderID
+{
+  System.Console.WriteLine($"{row}: {orderID}");
+  row++;
+}
+
+
+Console.WriteLine("");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("--- [Application Ended] ---------------------");
+Console.WriteLine("---------------------------------------------");
+Console.WriteLine("");
+
