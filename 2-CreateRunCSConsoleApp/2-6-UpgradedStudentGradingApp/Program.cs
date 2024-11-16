230200b6b7003df3e9939d743ee4761cfffb3724
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-17 05:52:53 +0700

    refactor: to calculate student score based on name - close #138

--- 2-CreateRunCSConsoleApp/2-6-UpgradedStudentGradingApp/Program.cs
+++ 2-CreateRunCSConsoleApp/2-6-UpgradedStudentGradingApp/Program.cs
@@ -11,6 +11,7 @@
 //  134-3b placeholder studentscore
 // 2-6-134-Use a foreach statement within the outer loop to iterate through the assignment scores array 
 //          and sum the values. #138
+//  134-4a foreach loop scores and sum values
 // 2-6-134-Use an updated algorithm within the outer loop to calculate the average exam score for each student. #139
 // 2-6-134-Use an if-elseif-else construct within the outer loop to evaluate the average exam score 
 //          and assign a letter grade automatically. #140
@@ -23,7 +24,7 @@
 using System;
 
 // initialize variables - graded assignments 
-// int currentAssignments = 5;
+int currentAssignments = 5;
 
 string[] student_names = ["oreo", "lilo", "ollie","momo"];  // 134-1a array student_name
 int[] oreo_scores   = {90,86,87,98,100};                    // 134-1b array score
@@ -31,57 +32,31 @@ int[] lilo_scores   = {92,89,81,96,90};
 int[] ollie_scores  = {90,85,87,98,68};
 int[] momo_scores   = {90,95,87,88,96}; 
 
+int[] current_scores = new int[5];
+int currentSum = 0;
+string current_student;
+
 foreach (string student in student_names)                   // 134-2a foreach student in student_name
 {
-  System.Console.WriteLine($"Student: {student}");          // 134-2b output: student
-  if (student == "oreo")                                    // 134-3a add if inside loop 
-  {                                                         // 134-3b placeholder studentscore                          
-    // oreo_scores              
+  // System.Console.WriteLine($"Student: {student}");          // 134-2b output: student
+  current_student = student;
+
+  if (current_student == "oreo")
+  {
+    current_scores = oreo_scores;
+  }   
+
+  foreach (int score in current_scores)
+  {
+    currentSum += score;
   }
 
-  if (student == "lilo")
-  {
-    // lilo_scores;
-  }
+  decimal currentScore = (decimal)currentSum / currentAssignments;
 
-  if (student == "ollie")
-  {
-    // ollie_scores;
-  }
-
-  if (student == "momo")
-  {
-    // momo_scores;
-  }
-  
+  Console.WriteLine("Student\t\tGrade\n");
+  Console.WriteLine($"{current_student}:\t\t" + currentScore + "\tA-");
+    
 }
 
-
-// int oreoSum = 0;
-// int liloSum = 0;
-// int ollieSum = 0;
-// int momoSum = 0;
-
-// decimal oreoScore;
-// decimal liloScore;
-// decimal ollieScore;
-// decimal momoScore;
-
-// oreoSum = oreo1 + oreo2 + oreo3 + oreo4 + oreo5;
-// liloSum = lilo1 + lilo2 + lilo3 + lilo4 + lilo5;
-// ollieSum = ollie1 + ollie2 + ollie3 + ollie4 + ollie5;
-// momoSum = momo1 + momo2 + momo3 + momo4 + momo5;
-
-// oreoScore = (decimal)oreoSum / currentAssignments;
-// liloScore = (decimal)liloSum / currentAssignments;
-// ollieScore = (decimal)ollieSum / currentAssignments;
-// momoScore = (decimal)momoSum / currentAssignments;
-
-// Console.WriteLine("Student\t\tGrade\n");
-// Console.WriteLine("oreo:\t\t" + oreoScore + "\tA-");
-// Console.WriteLine("lilo:\t\t" + liloScore + "\tB+");
-// Console.WriteLine("ollie:\t\t" + ollieScore + "\tB");
-// Console.WriteLine("momo:\t\t" + momoScore + "\tA-");
-
-// Console.WriteLine("Press the Enter key to continue");
-// Console.ReadLine();
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
