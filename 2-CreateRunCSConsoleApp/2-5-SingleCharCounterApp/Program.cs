7adb776868905a3db53565898e4ca10e67131704
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-17 02:22:56 +0700

    convert input string to char array - close #125

---
+++ 2-CreateRunCSConsoleApp/2-5-SingleCharCounterApp/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("--- Welcome to Tony's Fraud-Metrics App ---");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("");
+
+
+// 2-5-124-convert the string into a char array #125
+// 2-5-124-reverse the chars array (Array.Reverse) #126
+// 2-5-124-count the o's #127
+// 2-5-124-convert it back to a string and print the count of o's #128
+
+
+string input_str = "The quick brown fox jumps over the lazy dog.";
+char[] input_array = input_str.ToArray();
+
+Console.WriteLine("");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("--- Leaving Tony's Fraud-Metrics App ------");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("");
+
