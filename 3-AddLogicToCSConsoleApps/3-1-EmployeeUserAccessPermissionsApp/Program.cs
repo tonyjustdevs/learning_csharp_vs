ae47c15042d97a5b65eb9cf6d016b8c4bdc83040
tonyjustdevs <129901144+tonyjustdevs@users.noreply.github.com>
2024-11-19 04:21:09 +0700

    initial commit load starter-code

---
+++ 3-AddLogicToCSConsoleApps/3-1-EmployeeUserAccessPermissionsApp/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("--- Welcome to Tony's Employee-User-Permissions App ---");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine("");
+//////////////////////// START PROGRAM ////////////////////////
+
+
+
+//////////////////////// 1. PARAMS ////////////////////////////
+string permission = "Admin|Manager";  // starter-code
+int level = 55;                       // starter-code
+
+//////////////////////// 2. METHODS ///////////////////////////
+
+//////////////////////// 3. OUTPUT ////////////////////////////
+
+
+
+
+
+
+//////////////////////// END PROGRAM //////////////////////////
+Console.WriteLine("");
+Console.WriteLine("-------------------------------------------------------");
+Console.WriteLine("--- Leaving Tony's Employee-User-Permissions App ------");
+Console.WriteLine("-------------------------------------------------------");
+Console.WriteLine("");
+
